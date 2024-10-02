0a28543a0b22034177430150d44b21cf8b7d583e
mohamedddyasser <mohamed.yasser@intalio.com>
2024-10-02 14:45:58 +0300

    Revert "wwwwwwwww"
    This reverts commit 65060616ba7a2b6bcded0967960d06d4c8b9c325.

---
+++ customers/models/res_bank.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+
+class ResBank(models.Model):
+    _inherit = 'res.bank'
+    _description = 'Res Bank'
+
+    branches = fields.Many2many('res.bank.branch', string="Branches", domain=lambda self: [('res_bank_id', '=', self.id)])
+
