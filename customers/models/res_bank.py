e94a71c76d3fd775617b7912de80441ac8409eec
mohamedddyasser <mohamed.yasser@intalio.com>
2024-10-02 17:38:11 +0300

    xxx

--- customers/models/res_bank.py
+++ customers/models/res_bank.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 
 
 class ResBank(models.Model):
-    _inherit = 'res.bank'
+    _name = 'res.bank'
     _description = 'Res Bank'
 
     branches = fields.Many2many('res.bank.branch', string="Branches", domain=lambda self: [('res_bank_id', '=', self.id)])
