f004c624f6484f87800b32cb4e2b38cb794ac07b
mohamedddyasser <mohamed.yasser@intalio.com>
2024-10-02 14:23:06 +0300

    lol

--- customers/models/account_types.py
+++ customers/models/account_types.py
@@ -2,8 +2,7 @@ from odoo import models, fields
 
 
 class AccountTypes(models.Model):
-    _name = 'account.types'
-    _description = 'Account Types'
+    _name = "account.types"
+    _description = "Account Types"
 
     name = fields.Char(string="Name")
-
