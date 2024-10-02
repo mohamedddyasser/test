65060616ba7a2b6bcded0967960d06d4c8b9c325
mohamedddyasser <mohamed.yasser@intalio.com>
2024-10-02 14:20:34 +0300

    wwwwwwwww

--- customers/models/res_bank.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class ResBank(models.Model):
-    _inherit = 'res.bank'
-    _description = 'Res Bank'
-
-    branches = fields.Many2many('res.bank.branch', string="Branches", domain=lambda self: [('res_bank_id', '=', self.id)])
-
