13bed0204c5f720787d40f6122ef0ea111550da7
mohamedddyasser <mohamed.yasser@intalio.com>
2024-10-03 14:00:41 +0300

    solve test

--- customers/models/mail_activity.py
+++ customers/models/mail_activity.py
@@ -2,15 +2,14 @@ from odoo import models, fields, api
 
 
 class MailActivity(models.Model):
-    _inherit = 'mail.activity'
-    _description = 'Activity'
+    _inherit = "mail.activity"
+    _description = "Activity"
 
-    @api.depends('res_model', 'res_id', 'user_id')
+    @api.depends("res_model", "res_id", "user_id")
     def _compute_can_write(self):
-        valid_records = self._filter_access_rules('write')
+        valid_records = self._filter_access_rules("write")
         for record in self:
             if record.user_id.id == self.env.user.id:
                 record.can_write = record in valid_records
             else:
                 record.can_write = False
-                
--- customers/models/res_bank.py
+++ customers/models/res_bank.py
@@ -2,8 +2,11 @@ from odoo import models, fields
 
 
 class ResBank(models.Model):
-    _inherit = 'res.bank'
-    _description = 'Res Bank'
+    _inherit = "res.bank"
+    _description = "Res Bank"
 
-    branches = fields.Many2many('res.bank.branch', string="Branches", domain=lambda self: [('res_bank_id', '=', self.id)])
-
+    branches = fields.Many2many(
+        "res.bank.branch",
+        string="Branches",
+        domain=lambda self: [("res_bank_id", "=", self.id)],
+    )
